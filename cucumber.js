80f94e2f1f2184cca2d7012be0029dbda49f9f4d
cristianHdezOmni <cristian.hernandez@omni.pro>
2025-09-10 21:54:57 -0500

    Demo serenityBDD - serenity-report.yml ajuste

--- cucumber.js
+++ cucumber.js
@@ -1,9 +1,12 @@
 module.exports = {
   default: {
-    require: ['src/StepDefinitions/**/*.ts'],
-    requireModule: ['ts-node/register'],    
-    //paths: ['src/features/registration.feature'],
+    requireModule: ['ts-node/register'], 
+    require: [
+      'src/StepDefinitions/**/*.ts',
+      'src/support/**/*.ts',
+    ],  
     paths: ['src/Resource/features/**/*.feature'],
+    format: ['@serenity-js/cucumber'],
     // Configuración de tags
     tags: process.env.TAGS || '@smoke' // Por defecto ejecuta solo @smoke
   }
