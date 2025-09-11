a7431ad679f2f626025124261123cb146964cf03
cristianHdezOmni <cristian.hernandez@omni.pro>
2025-09-11 08:23:29 -0500

    SerenityBDD - Run GitHub Pages -ajuste v4

---
+++ src/support/hooks.ts
@@ -0,0 +1,16 @@
+import { BeforeAll } from '@cucumber/cucumber';
+import { configure } from '@serenity-js/core';
+import * as path from 'path';
+
+BeforeAll(() => {
+    configure({
+        crew: [
+            [ '@serenity-js/serenity-bdd', {
+                specDirectory: path.resolve(__dirname, '../Resource/features')
+            } ],
+            [ '@serenity-js/core:ArtifactArchiver', {
+                outputDirectory: path.resolve(__dirname, '../../target/site/serenity')
+            } ],
+        ],
+    });
+});
