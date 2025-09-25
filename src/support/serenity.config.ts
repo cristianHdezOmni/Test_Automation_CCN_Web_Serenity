3e1886a3d0f7654d481d775ddf2e345f78d559ae
cristianHdezOmni <cristian.hernandez@omni.pro>
2025-09-24 20:11:26 -0500

    SerenityBDD - Regresion - headless=false - Ajuste

--- src/support/serenity.config.ts
+++ src/support/serenity.config.ts
@@ -18,14 +18,44 @@
 //   // Playwright integration is handled via abilities in step definitions/hooks
 // };
 
-import { ConsoleReporter } from '@serenity-js/console-reporter';
-import SerenityBDDReporter from '@serenity-js/serenity-bdd';
-import { AfterStep, BeforeAll, Before, After, setWorldConstructor } from '@cucumber/cucumber';
-import { configure, ArtifactArchiver, actorCalled } from '@serenity-js/core';
+
+import { AfterStep, Before, setWorldConstructor } from '@cucumber/cucumber';
+import { configure,  actorCalled } from '@serenity-js/core';
 import { BrowseTheWebWithPlaywright } from '@serenity-js/playwright';
 import { chromium } from 'playwright';
 import { TakeScreenshot } from '@serenity-js/web';
 import * as path from 'path';
+
+
+
+// Before({ timeout: 30000 }, async function () {
+//     // 🎭 Configuración del navegador basada en variable de entorno
+//     //const headless = process.env.HEADLESS === 'true' || false;
+//     const headless = process.env.HEADLESS !== 'false';
+//     const browser = await chromium.launch({
+//         channel: 'chrome', // 👈 esto lanza Google Chrome real
+//         headless: headless     // 👀 configurable según el ambiente
+//     });
+//     const context = await browser.newContext();
+//     const page = await context.newPage();
+//     this.actor = actorCalled('User QA Test Automation').whoCan(
+//         BrowseTheWebWithPlaywright.usingPage(page)
+//     );
+
+//     // Ensure browser and context are closed after tests
+//     this.cleanup = async () => {
+//         await context.close();
+//         await browser.close();
+//     };
+// });
+
+// Hook to clean up resources after each scenario
+
+// After({ timeout: 40000 },async function () {
+//     if (this.cleanup) {
+//         await this.cleanup();
+//     }
+// });
 
 export class CustomWorld {
   actor: any;
@@ -33,47 +63,29 @@ export class CustomWorld {
 
 setWorldConstructor(CustomWorld);
 
-Before({ timeout: 30000 }, async function () {
-    // 🎭 Configuración del navegador basada en variable de entorno
-    //const headless = process.env.HEADLESS === 'true' || false;
-    const headless = process.env.HEADLESS !== 'false';
-    const browser = await chromium.launch({
-        channel: 'chrome', // 👈 esto lanza Google Chrome real
-        headless: headless     // 👀 configurable según el ambiente
-    });
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    this.actor = actorCalled('User QA Test Automation').whoCan(
-        BrowseTheWebWithPlaywright.usingPage(page)
-    );
-
-    // Ensure browser and context are closed after tests
-    this.cleanup = async () => {
-        await context.close();
-        await browser.close();
-    };
-});
-
-// Hook to clean up resources after each scenario
-
-After({ timeout: 40000 },async function () {
-    if (this.cleanup) {
-        await this.cleanup();
-    }
-});
-
-BeforeAll(() => {
-    configure({
-        crew: [
-          // ConsoleReporter.withDefaultColourSupport(),
-           // ✅ Reporter con procesadores requeridos
-           // ✅ Reporter estándar
-           //SerenityBDDReporter(),
-           // ✅ Archiver usando ruta por defecto
-          // ArtifactArchiver.storingArtifactsAt('target/site/serenity'),
-            [ '@serenity-js/serenity-bdd', {
-                specDirectory: path.resolve(__dirname, '../Resource/features'),
-                reporter: {
+Before({ timeout: 30000 }, async function(){
+  // 🎭 Configuración del navegador basada en variable de entorno
+  //const headless = process.env.HEADLESS === 'true' || false;
+  const headless = process.env.HEADLESS !== 'false';
+  const browser = await chromium.launch({
+    channel: 'chrome', // 👈 esto lanza Google Chrome real
+    headless: headless     // 👀 configurable según el ambiente
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  this.actor = actorCalled('User QA Test Automation').whoCan(
+    BrowseTheWebWithPlaywright.usingPage(page)
+  );
+  // Ensure browser and context are closed after tests
+  this.cleanup = async () => {
+    await context.close();
+    await browser.close();
+  };    
+  configure({
+    crew: [
+      [ '@serenity-js/serenity-bdd', {
+        specDirectory: path.resolve(__dirname, '../Resource/features'),
+        reporter: {
                     includeAbilityDetails: true,
                     requirementsDirectory: path.resolve(__dirname, '../Resource/features'),
                     requirementTypes: [
@@ -90,11 +102,14 @@ BeforeAll(() => {
     });
 });
 
-AfterStep(async function () {
+AfterStep({ timeout: 40000 }, async function () {
     if (this.actor) {
         await this.actor.attemptsTo(
             TakeScreenshot.of('step')
         );
+    }
+     if (this.cleanup) {
+        await this.cleanup();
     }
 });
 
